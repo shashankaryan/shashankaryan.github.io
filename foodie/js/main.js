57c6c708229323eab2ab8af222a404ae9ba4b367
Shashank Aryan <aryanshashank31@gmail.com>
2018-01-24 06:02:24 +0530

    Foodie design 5 page completed

--- foodie/js/main.js
+++ foodie/js/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
+
+  // ONE PAGE SCROLL INIT
   $(".main").onepage_scroll({
     sectionContainer: "section",
+    easing: "ease",
+    animationTime: 1000,
     loop: true,
     responsiveFallback: false
   });
+
+  var parallaxSettings = {
+    initialOpacity: 1, //from 0 to 1, e.g. 0.34 is a valid value. 0 = transparent, 1 = Opaque
+    opacitySpeed: 0.1, //values from 0.01 to 1 -> 0.01: slowly appears on screen; 1: appears as soon as the user scrolls 1px
+    pageLoader: true
+  };
+
+  // parallaxImgScroll(parallaxSettings);
+
 });
