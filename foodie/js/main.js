4d4d8fc80a97ad9af16ddf3e36947918cc41c5c7
Shashank Aryan <aryanshashank31@gmail.com>
2018-01-25 19:09:19 +0530

    Crousel modified.

--- foodie/js/main.js
+++ foodie/js/main.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
 
-    //activate wow.js
-     new WOW().init();
+  //activate wow.js
+  new WOW().init();
 
-    //activate fullpage.js
-    $('#fullpage').fullpage({
-      scrollBar: true,
-      navigation: true,
-      navigationTooltips: ['01', '02', '03', '04', '05', '06'],
-      loopBottom: true,
-      sectionSelector: 'section'
-    });
+  //activate fullpage.js
+  $('#fullpage').fullpage({
+    scrollBar: true,
+    navigation: true,
+    navigationTooltips: [
+      '01',
+      '02',
+      '03',
+      '04',
+      '05',
+      '06'
+    ],
+    loopBottom: true,
+    sectionSelector: 'section'
+  });
 
-    // Flickity Settings
-    $('.carousel').flickity().flickity('next').flickity( 'select', 3 );
+  // Initialising Slick Carousel
+  $('.carousel').slick();
 });
